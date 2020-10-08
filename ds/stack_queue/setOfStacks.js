6d84a546018e525a13f42267789a9b17269974b4
Fan Yang <yangfcm16@gmail.com>
2020-10-08 23:51:41 +1030

    fix typo

--- ds/stack_queue/setOfStacks.js
+++ ds/stack_queue/setOfStacks.js
@@ -1,7 +1,7 @@
 /**
- * Image a (literal) stack of plates. If the stack goes too high, it might topple.
+ * Imagine a (literal) stack of plates. If the stack goes too high, it might topple.
  * Therefore, in real life, we would start a new stack when the previous stack exceeds a threshold.
- * Implement a data structure SetOfStacks to minic this.
+ * Implement a data structure SetOfStacks to mimic this.
  * SetOfStacks should be composed of several stacks and should create a new stack once the previous one exceeds capacity.
  * SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single stack
  * Also implement a function popAt(index) to perform pop operation on a specific sub-stack.
