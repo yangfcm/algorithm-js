2445bbb48347ee72c6a11e765785fba7fa9ef518
Fan Yang <yangfcm16@gmail.com>
2020-11-22 21:58:02 +1030

    fix typo

--- algo/dynamic/maxProfit.js
+++ algo/dynamic/maxProfit.js
@@ -1,7 +1,7 @@
 /**
  * Say you have an array for which the ith element is the price of a given stock on day i.
  * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
- * e.g. [7, 1, 5, 3, 6, 4] => 7 ( (5-1) + (6-3) = 4 )
+ * e.g. [7, 1, 5, 3, 6, 4] => 7 ( (5-1) + (6-3) = 7 )
  * @param {number[]} prices
  * @return {number}
  */
