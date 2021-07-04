d0d2483a2204eabbacfec5a39de5185adc6cf012
Fan Yang <yangfcm16@gmail.com>
2021-07-04 23:52:34 +0930

    array/chunk: use and test two solutions.

--- algo/array/__test__/chunk.test.js
+++ algo/array/__test__/chunk.test.js
@@ -1,48 +1,69 @@
-const chunk = require("../chunk");
+const { solution1, solution2 } = require("../chunk");
 
 describe("Test chunk.js", () => {
-  test("chunk function defined", () => {
-    expect(chunk).toBeDefined();
+  test("solution functions defined", () => {
+    expect(solution1).toBeDefined();
+    expect(solution2).toBeDefined();
   });
 
   test("chunk divides an array of 10 elements with chunk size 2", () => {
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const chunked = chunk(arr, 2);
-
-    expect(chunked).toEqual([
+    const expected = [
       [1, 2],
       [3, 4],
       [5, 6],
       [7, 8],
-      [9, 10]
-    ]);
+      [9, 10],
+    ];
+    const size = 2;
+
+    const chunked1 = solution1(arr, size);
+    expect(chunked1).toEqual(expected);
+
+    const chunked2 = solution2(arr, size);
+    expect(chunked2).toEqual(expected);
   });
 
   test("chunk divides an array of 3 elements with chunk size 1", () => {
     const arr = [1, 2, 3];
-    const chunked = chunk(arr, 1);
+    const expected = [[1], [2], [3]];
+    const size = 1;
 
-    expect(chunked).toEqual([[1], [2], [3]]);
+    const chunked1 = solution1(arr, size);
+    expect(chunked1).toEqual(expected);
+
+    const chunked2 = solution2(arr, size);
+    expect(chunked2).toEqual(expected);
   });
 
   test("chunk divides an array of 5 elements with chunk size 3", () => {
     const arr = [1, 2, 3, 4, 5];
-    const chunked = chunk(arr, 3);
-
-    expect(chunked).toEqual([
+    const expected = [
       [1, 2, 3],
-      [4, 5]
-    ]);
+      [4, 5],
+    ];
+    const size = 3;
+
+    const chunked1 = solution1(arr, size);
+    expect(chunked1).toEqual(expected);
+
+    const chunked2 = solution2(arr, size);
+    expect(chunked2).toEqual(expected);
   });
 
   test("chunk divides an array of 13 elements with chunk size 5", () => {
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-    const chunked = chunk(arr, 5);
-
-    expect(chunked).toEqual([
+    const expected = [
       [1, 2, 3, 4, 5],
       [6, 7, 8, 9, 10],
-      [11, 12, 13]
-    ]);
+      [11, 12, 13],
+    ];
+    const size = 5;
+
+    const chunked1 = solution1(arr, size);
+    expect(chunked1).toEqual(expected);
+
+    const chunked2 = solution2(arr, size);
+    expect(chunked2).toEqual(expected);
   });
 });
--- algo/array/chunk.js
+++ algo/array/chunk.js
@@ -7,41 +7,40 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-// Solution 2:
-// Create an empty array to hold chunks called 'chunked'
-// Create 'index' start at 0
-// While index is less than array.length
-// Push a slice of length 'size' from 'array' into 'chunked' using slice() built-in method of array
-// Add 'size' to 'index'
-function chunk(array, size) {
-  const chunked = [];
-  let index = 0;
-  while(index < array.length) {
-    chunked.push(array.slice(index, index+size));
-    index = index+size;
-  }
-  return chunked;
-}
-
 // Solution 1:
 // Create an empty array to hold chunks called 'chunked'
 // For each element in the original 'unchunked' array, retrieve the last element in 'chunked'
 // If last element does not exist or if its length is equal to chunk size, push a new chunk
 // into 'chunked' with the current element.
 // Otherwise, add the current element into the current chunk.
-/*
-function chunk(array, size) {
+
+function solution1(array, size) {
   const chunked = [];
-  for(let element of array) {
+  for (let element of array) {
     const lastSubArr = chunked[chunked.length - 1];
-    if(!lastSubArr || lastSubArr.length === size) {
-      chunked.push([element]);  // Create a new subarray
+    if (!lastSubArr || lastSubArr.length === size) {
+      chunked.push([element]); // Create a new subarray
     } else {
       lastSubArr.push(element); // Push the element to the current subarray
     }
   }
-
   return chunked;
 }
-*/
-module.exports = chunk;
+
+// Solution 2:
+// Create an empty array to hold chunks called 'chunked'
+// Create 'index' start at 0
+// While index is less than array.length
+// Push a slice of length 'size' from 'array' into 'chunked' using slice() built-in method of array
+// Add 'size' to 'index'
+function solution2(array, size) {
+  const chunked = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index = index + size;
+  }
+  return chunked;
+}
+
+module.exports = { solution1, solution2 };
