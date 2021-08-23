80dfdfbc385b153aabb6450480398594a3c41f0d
Fan Yang <yangfcm16@gmail.com>
2021-08-24 00:34:32 +0930

    add examples to canConstruct.

--- algo/dynamic/canConstruct.js
+++ algo/dynamic/canConstruct.js
@@ -3,6 +3,8 @@
  * The function should return a boolean indicating whether or not the target can be
  * constructed by concatenating elements of the strs array.
  * You may use elements in strs as many times as needed.
+ * e.g. You can use elements in ["ab", "abc", "cd", "def", "abcd"] to construct string "abcdef" ("abc" + "def"), so function returns true
+ * But there is no way to construct "abcef", so function returns false.
  */
 function solution1(target, strs) {
   if (target === "") {
