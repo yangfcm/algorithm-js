aee055ac14fcb78ada776cce4fe91498ce891f71
Fan Yang <yangfcm16@gmail.com>
2022-06-15 14:39:53 +0930

    Add fradulentActivitynotifications file.

---
+++ sorting/fradulentActivityNotifications.js
@@ -0,0 +1,9 @@
+/**
+ * @name fradulentActivityNotifications
+ * @param {array} expenditure An array that represents how much is spent for consecutive days.
+ * @param {number} d From which day, notification is started.
+ * @returns {number} How many notifications are triggered.
+ */
+function fradulentActivityNotifications(expenditure, d) {}
+
+module.exports = fradulentActivityNotifications;
