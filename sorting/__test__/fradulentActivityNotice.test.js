dcde72550b057a3433e310487e00f63d007c8d6a
Fan Yang <yangfcm16@gmail.com>
2022-07-05 22:35:42 +0930

    Scope the variables.

--- sorting/__test__/fradulentActivityNotice.test.js
+++ sorting/__test__/fradulentActivityNotice.test.js
@@ -2,11 +2,11 @@ const fradulentActivityNotice = require("../fradulentActivityNotice");
 
 describe("Test fradulentActivityNotice.js", () => {
   const exp1 = [2, 3, 4, 2, 3, 6, 8, 4, 5];
-  d1 = 5;
+  const d1 = 5;
   const exp2 = [1, 2, 3, 4, 4];
-  d2 = 4;
+  const d2 = 4;
   const exp3 = [10, 20, 30, 40, 50];
-  d3 = 3;
+  const d3 = 3;
 
   test("Return correct count of notice", () => {
     expect(fradulentActivityNotice(exp1, d1)).toBe(2);
