472b41b3791e9c831b4b4024c49e97c22681b9ed
Fan Yang <yangfcm16@gmail.com>
2022-12-12 23:25:44 +1030

    Add question of largestRectangle.

---
+++ ds/stack_queue/largestRectangle.js
@@ -0,0 +1,22 @@
+/**
+ * @name largestRectangle
+ * @description Given an array of integers heights representing the histogram's bar height
+ * where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+ * @example heights = [2, 1, 5, 6, 2, 3] -> 10
+ *        _
+ *      _| |
+ *     |+|+|
+ *     |+|+|  _
+ *  _  |+|+|_| |
+ * | |_|+|+| | |
+ * |2|1|5|6|2|3|
+ * -------------
+ * The above is the a histogram where width of each bar is 1.
+ * The largest rectangle is the one created by 5 and 6, which has an area of 2 * 5 = 10 units.
+ * @param {array} heights
+ * @returns The area of the largest rectangle.
+ * @source https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+ */
+function solution(heights) {}
+
+module.exports = { solution };
