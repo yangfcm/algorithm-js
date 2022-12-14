1942ad56378e1527d6025bb3c74a078354143531
Fan Yang <yangfcm16@gmail.com>
2022-12-14 23:22:23 +1030

    Add a more straightforward but slower solution to largestRectangle.

--- ds/__test__/stack_queue/largestRectangle.test.js
+++ ds/__test__/stack_queue/largestRectangle.test.js
@@ -1,4 +1,4 @@
-const { solution } = require("../../stack_queue/largestRectangle");
+const { solution, solution2 } = require("../../stack_queue/largestRectangle");
 
 describe("Test largestRectangle.js", () => {
   test("Solution should return the area of the largest rectangle", () => {
@@ -6,5 +6,12 @@ describe("Test largestRectangle.js", () => {
     expect(solution([2, 1, 5, 6, 2, 3])).toBe(10);
     expect(solution([11, 11, 10, 10, 10])).toBe(50);
     expect(solution([1, 3, 5, 9, 11])).toBe(18);
+  });
+
+  test("Solution 2 should return the area of the largest rectangle", () => {
+    expect(solution2([1, 2, 3, 4, 5])).toBe(9);
+    expect(solution2([2, 1, 5, 6, 2, 3])).toBe(10);
+    expect(solution2([11, 11, 10, 10, 10])).toBe(50);
+    expect(solution2([1, 3, 5, 9, 11])).toBe(18);
   });
 });
--- ds/stack_queue/largestRectangle.js
+++ ds/stack_queue/largestRectangle.js
@@ -15,9 +15,31 @@
  * The largest rectangle is the one created by 5 and 6, which has an area of 2 * 5 = 10 units.
  * @param {array} heights
  * @returns The area of the largest rectangle.
+ * @solution Iterate through the heights array, for each bar(height), calculate the area with
+ * this element as the smallest bar in the rectangle. Then, we can calculate the such area for every bar and find out the maximum of all such areas.
  * @source https://leetcode.com/problems/largest-rectangle-in-histogram/description/
  */
-function solution(heights) {
+function solution1(heights) {
+  let maxArea = 0;
+  for (let i = 0; i < heights.length; i++) {
+    const height = heights[i];
+    let width = 1;
+    for (let j = i - 1; j >= 0; j--) {
+      if (heights[j] >= height) width++;
+      else break;
+    }
+    for (let k = i + 1; k < heights.length; k++) {
+      if (heights[k] >= height) width++;
+      else break;
+    }
+    const area = height * width;
+    if (area > maxArea) maxArea = area;
+  }
+  return maxArea;
+}
+
+// This solution uses the idea of stacks to improve the above algorithm
+function solution2(heights) {
   const stack = [];
   let maxArea = 0;
   let index = 0;
@@ -46,4 +68,4 @@ function solution(heights) {
   return maxArea;
 }
 
-module.exports = { solution };
+module.exports = { solution1, solution2 };
