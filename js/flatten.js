c1b19f54396bc0161a58a2de6a1ce095c0087823
yangfcm <yangfcm@outlook.com>
2023-10-02 21:54:19 +1030

    Implement the first solution to flatten.

--- js/flatten.js
+++ js/flatten.js
@@ -5,15 +5,29 @@
  * @example [1, 2, 3] -> [1, 2, 3]
  * [1, [2, 3]] -> [1, 2, 3]
  * [1, [2, [3, [4, [5]]]]] -> [1, 2, 3, 4, 5]
+ */
+
+/**
  * @param {Array} arr
  * @returns {Array}
+ * @solution Simple iteration. Use an varaible to store the flattened array.
+ * Loop through the array with a while loop and take out an item from the array once at a time.
+ * If the item is not an arrary, just put it into the result array.
+ * If the item is an array, we get all the items and put them back to the array. (using spread operator ...)
  */
-function flatten(arr) {
+function flatten1(arr) {
   const flatArr = [];
   const copyArr = arr.slice();
 
-  for (let el in arr) {
+  while (copyArr.length > 0) {
+    const item = copyArr.shift();
+    if (Array.isArray(item)) {
+      copyArr.unshift(...item);
+    } else {
+      flatArr.push(item);
+    }
   }
+  return flatArr;
 }
 
-module.exports({ flatten });
+module.exports({ flatten1 });
