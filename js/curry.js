b26c216feb9dffac0158629f59cf725f4339c722
Fan Yang <yangfcm16@gmail.com>
2023-12-19 11:37:30 +1030

    Complete comment to curry function.

--- js/curry.js
+++ js/curry.js
@@ -1,9 +1,22 @@
 /**
- *
+ * @name curry
+ * @description Create a function that accepts arguments of func and either invokes func returning its result,
+ * if at least arity number of arguments have been provided,
+ * or returns a function that accepts the remaining func arguments, and so on.
+ * The arity of func may be specified if func.length is not sufficient.
+ * @example 
+ *  var abc = function(a, b, c) {
+      return [a, b, c];
+    };    
+    var curried = curry(abc);
+
+    curried(1)(2)(3); -> [1, 2, 3]
+    curried(1, 2)(3); -> [1, 2, 3]
+    curried(1, 2, 3); -> [1, 2, 3]
  * @param {function} func a function to be curried
  * @param {number} arity
+ * @returns {function}
  */
-
 function curry(func, arity = func.length) {
   return function curried(...args) {
     if (args.length >= arity) {
