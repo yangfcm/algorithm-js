814f1769df039e9995733eb4f055d36d5e2dbf67
Fan Yang <yangfcm16@gmail.com>
2024-03-01 13:33:49 +1030

    Add problem of integerPartition.

---
+++ algo/math/integerPartition.js
@@ -0,0 +1,16 @@
+/**
+ * @name integerPartition
+ * @description Given an integer, work out all possible unique ways to represent n as sum of positive integers.
+ * @example n = 1 -> 1
+ * n = 2 -> 2, 1 + 1
+ * n = 3 -> 3, 2 + 1, 1 + 1 + 1
+ * n = 4 -> 4, 3 + 1, 2 + 2, 2 + 1 + 1, 1 + 1 + 1 + 1
+ * n = 5 -> 5, 4 + 1, 3 + 2, 3 + 1 + 1, 2 + 2 + 1, 2 + 1 + 1 + 1, 1 + 1 + 1 + 1 + 1
+ * @param {number} n
+ * @solution @TODO
+ * @returns {array} An array of integers who can sum up to n.
+ * @solution
+ */
+function solution(n) {}
+
+module.exports = { solution };
