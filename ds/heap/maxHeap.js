ee9b5ed979dc0bbc5ae5490bfb644d4e7d9b4ae4
Fan Yang <yangfcm@outlook.com>
2024-06-08 16:17:19 +0930

    Add parent helper method.

--- ds/heap/maxHeap.js
+++ ds/heap/maxHeap.js
@@ -3,6 +3,7 @@
  * @description Implement a max heap class and its relevant methods:
  * 1. insert - Insert an element to a heap.
  * 2. print - Return all the elements in the heap.
+ * 3. remove - Remove the largest element from the heap and reorder it.
  * @source https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/insert-an-element-into-a-max-heap
  */
 class MaxHeap {
@@ -18,7 +19,7 @@ class MaxHeap {
     this.heap.push(num);
     let currentIndex = this.heap.length - 1;
     while (currentIndex > 0) {
-      const parentIndex = Math.floor((currentIndex - 1) / 2);
+      const parentIndex = this._parent(currentIndex);
       if (this.heap[currentIndex] > this.heap[parentIndex]) {
         [this.heap[currentIndex], this.heap[parentIndex]] = [
           this.heap[parentIndex],
@@ -29,6 +30,10 @@ class MaxHeap {
         break;
       }
     }
+  }
+
+  _parent(i) {
+    return Math.floor((i - 1) / 2);
   }
 }
 
